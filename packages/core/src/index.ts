3ed0cc6903ef3b1af951762e60a680f308af2443
Tomasz Morawski <tomasz.morawski@ipfdigital.com>
2026-08-30 08:43:48 +0200

    feat(core): add Button/IconButton/OutlineButton with slot + data-* contract

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,3 +2,11 @@ export { cn } from "./utils/cn";
 export { useControllableState } from "./hooks/useControllableState";
 export { useCloseOnEscape } from "./hooks/useCloseOnEscape";
 export { useCloseOnOutsideClick } from "./hooks/useCloseOnOutsideClick";
+export { Button, IconButton, OutlineButton } from "./components/Button";
+export type {
+  ButtonProps,
+  ButtonIntent,
+  ButtonSize,
+  ButtonVariant,
+  ButtonSlot,
+} from "./components/Button";
